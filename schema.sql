dce7d74f9a9943ff6ff0fc3fdc93e247b84d6f68
Bhargav Mandadi <bhargav_mandadi@thbs.india.com>
2022-03-22 16:11:57 +0530

    schema

---
+++ schema.sql
@@ -0,0 +1,6 @@
+create schema restaurant;
+use restaurant;
+
+create table userlogin(userid INT, username varchar(20), pass varchar(20));
+
+create table restaurant(rest_id varchar(10),res_name varchar(30),location varchar(30));
